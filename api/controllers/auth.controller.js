5a2be0530c1fba6212fa6ab695b08ebdef3026e4
Kidus Asebe <kidusasebe2019@gmail.com>
2024-07-11 13:41:50 +0300

    username appearance

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -53,14 +53,15 @@ export const google = async (req, res, next) => {
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+
+      const [username, domain] = req.body.email.split('@');
       const newUser = new User({
-        username:
-          req.body.name.split(' ').join('').toLowerCase() +
-          Math.random().toString(36).slice(-8),
+        username,
         email: req.body.email,
         password: hashedPassword,
         profilePicture: req.body.photo,
       });
+
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: hashedPassword2, ...rest } = newUser._doc;
