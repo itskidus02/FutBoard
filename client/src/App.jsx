fe2af95eade34aea67c1f4a5007ff544b5df312e
Kidus Asebe <kidusasebe2019@gmail.com>
2024-07-13 19:39:02 +0300

    modified app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+import CreateTableAndClub from "./pages/CreateTableAndClub";
 
 export default function App() {
   return (
-    
-
     <BrowserRouter>
-  
       <Header />
-      <div className='mt-16'>
-
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-        </Route>
-      </Routes>
+      <div className="mt-16">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-table-team" element={<CreateTableAndClub />} />
+          </Route>
+        </Routes>
       </div>
     </BrowserRouter>
-    
   );
 }
