79e8dea8421f706f41b6f54e1d9e8055b6edae4a
Kidus Asebe <kidusasebe2019@gmail.com>
2024-07-26 16:30:07 +0300

    added position to the table

--- client/src/pages/DisplayTable.jsx
+++ client/src/pages/DisplayTable.jsx
@@ -55,7 +55,7 @@ const DisplayTable = () => {
   return (
     <main>
       {table && (
-        <div className="max-w-4xl mx-auto p-3 my-7">
+        <div className="w-3/4 mx-auto p-3 my-7">
           <h1 className="text-3xl font-semibold text-center my-7">{table.name}</h1>
           {creator && (
             <div className="flex ring-2 p-2 ring-[#00ed64] rounded-lg flex-col gap-2">
@@ -69,7 +69,8 @@ const DisplayTable = () => {
             <table className="min-w-full bg-white border-collapse">
               <thead>
                 <tr>
-                  <th className="py-2 px-4 border-b">Club Name</th>
+                  <th className="py-2 px-4 border-b">Position</th>
+                  <th className="py-2 px-4 border-b">Club</th>
                   <th className="py-2 px-4 border-b">Played</th>
                   <th className="py-2 px-4 border-b">Won</th>
                   <th className="py-2 px-4 border-b">Lost</th>
@@ -83,6 +84,7 @@ const DisplayTable = () => {
               <tbody>
                 {table.clubs.map((club, index) => (
                   <tr key={index} className="text-center">
+                    <td className="py-2 px-4 border-b">{index + 1}</td>
                     <td className="py-2 px-4 border-b flex items-center">
                       {club.clubId.logoUrl && (
                         <img
