1f1c75f677d99ef6a60c6b58ff010ad323212bfd
Kidus Asebe <kidusasebe2019@gmail.com>
2024-08-01 11:28:59 +0300

    .

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className='h-screen flex justify-center items-center text-4xl'> ፎሮፎር </div>
+    <div className='mt-40 flex flex-col gap-3 justify-center items-center '> 
+    
+    
+    <span className='text-4xl'>ፎሮፎር </span>
+     <button className='ring-2 ring-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white font-bold py-2 px-4 rounded'>
+          <Link to="/">Home</Link>
+        </button>
+    
+    
+    
+    </div>
   )
 }
 
