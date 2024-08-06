0c0e5eb30fd16a907895eb1a64ff56723b751314
Kidus Asebe <kidusasebe2019@gmail.com>
2024-08-06 15:50:14 +0300

    sign in page setup

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -41,40 +41,47 @@ export default function SignIn() {
     }
   };
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+    <div className="flex justify-center items-center p-3 max-w-screen-xl mx-auto">
+    <div className="flex-1">
+      <h1 className="lg:text-[140px] md:text-[100px] font-bold">FutBoard.</h1>
+    </div>
+    <div className="flex-1 max-w-lg">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
-          type='email'
-          placeholder='Email'
-          id='email'
-          className='bg-slate-100 p-3 rounded-lg'
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
         />
         <input
-          type='password'
-          placeholder='Password'
-          id='password'
-          className='bg-slate-100 p-3 rounded-lg'
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
         />
         <button
           disabled={loading}
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
         <OAuth />
       </form>
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
         <p>Dont Have an account?</p>
-        <Link to='/sign-up'>
-          <span className='text-blue-500'>Sign up</span>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign up</span>
         </Link>
       </div>
-      <p className='text-red-700 mt-5'>
+      <p className="text-red-700 mt-5">
         {error ? error.message || 'Something went wrong!' : ''}
       </p>
     </div>
+  </div>
+  
+  
   );
 }
