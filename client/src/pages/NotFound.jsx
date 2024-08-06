34bad00213e92712755021dc8852db18326cc518
Kidus Asebe <kidusasebe2019@gmail.com>
2024-08-06 23:56:48 +0300

    notfound page done

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className='mt-40 flex flex-col gap-3 justify-center items-center '> 
-    
-    
-    <span className='text-4xl'>ፎሮፎር </span>
-     <button className='ring-2 ring-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white font-bold py-2 px-4 rounded'>
-          <Link to="/">Home</Link>
-        </button>
-    
-    
-    
+    <div className="flex flex-col items-center justify-center font-fraunces mt-32 bg-white text-center mx-auto p-4">
+      <div className="text-4xl sm:text-5xl text-[#00684A] lg:text-[52px]">404</div>
+      <div className="mt-10 font-fraunces text-5xl md:text-[50px] text-[#00684A] lg:text-[72px] mb-10">Page not found</div>
+      <div className="mt-9">
+        <Link to="/" className="text-xl bg-[#00684A] hover:ring-2 hover:ring-[#00684A] hover:bg-white hover:text-[#00684A] font-fraunces rounded-lg text-white px-4 lg:px-5 md:px-10 py-2 lg:py-2.5 transition-all sm:text-xl">
+          Home
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
