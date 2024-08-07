115fe0636485b742e4f1d84116fa918e88baeb12
Kidus Asebe <kidusasebe2019@gmail.com>
2024-08-07 15:58:55 +0300

    update table done

--- client/src/pages/UpdateTableAndClub.jsx
+++ client/src/pages/UpdateTableAndClub.jsx
@@ -8,6 +8,7 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import plus from "../assets/plus.svg"
 
 export default function UpdateTableAndClub() {
   const [numTeams, setNumTeams] = useState(0);
@@ -66,6 +67,8 @@ export default function UpdateTableAndClub() {
     setExistingTeamLogos([...existingTeamLogos, null]);
     setNumTeams(numTeams + 1);
   };
+
+ 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -162,76 +165,99 @@ export default function UpdateTableAndClub() {
 
   return (
     <main className="p-3 max-w-6xl mx-auto">
-      <h1 className="text-3xl flex ml-4 items-center gap-3 font-semibold text-center my-7">
-        <div className="bg-[#00684A] rounded-lg h-14">.</div>
-        <span className="lg:text-[28.125px] md:text-[20.125px] font-fraunces text-[#00684A] text-[20.125px]">
-          Update Table and Clubs
-        </span>
-      </h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <div className="flex gap-4">
-          <div className="flex gap-3 flex-col w-1/2">
-            <span className="mt-4 font-poppins font-semibold lg:text-2xl">
-              Table Name
-            </span>
-            <input
-              type="text"
-              placeholder="Table Name"
-              className="border ring-2 ring-[#00684A] p-3 font-poppins font-light rounded-lg"
-              value={tableName}
-              onChange={handleTableNameChange}
-              required
-            />
-          </div>
-          <div className="flex flex-col gap-3 w-1/2">
-            <span className="mt-4 lg:text-2xl font-poppins font-semibold">
-              Number of Teams
-            </span>
+    <h1 className="text-3xl flex ml-4 items-center gap-3 font-semibold text-center my-7">
+      <div className="bg-[#00684A] rounded-lg h-14">.</div>
+      <span className="lg:text-[28.125px] md:text-[20.125px] font-fraunces text-[#00684A] text-[20.125px]">
+        Update Table and Clubs
+      </span>
+    </h1>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+      <div className="flex flex-col lg:flex-row gap-4">
+        <div className="flex gap-3 flex-col lg:w-1/2 w-full">
+          <span className="mt-4 font-poppins font-semibold lg:text-2xl">
+            Table Name
+          </span>
+          <input
+            type="text"
+            placeholder="Table Name"
+            className="border ring-2 ring-[#00684A] p-3 font-poppins font-light rounded-lg"
+            value={tableName}
+            onChange={handleTableNameChange}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-3 lg:w-1/2 w-full">
+          <span className="mt-4 lg:text-2xl font-poppins font-semibold">
+            Number of Teams
+          </span>
+          <div className="flex gap-2 items-center">
             <input
               type="number"
               placeholder="Number of Teams"
-              className="border ring-2 ring-[#00684A] p-3 rounded-lg"
+              className="border ring-2 ring-[#00684A] p-3 rounded-lg flex-grow"
               value={numTeams}
               disabled
               required
             />
-           
+            <button
+              type="button"
+              onClick={handleAddTeam}
+              className="px-3 py-4 bg-[#00684A] rounded-lg  flex-shrink-0"
+            >
+              
+              <img
+              className="w-5 h-5"
+              src={plus}
+              />
+            </button>
+
+            {/* // if remove button is needed */}
+            {/* <button
+              type="button"
+              onClick={handleAddTeam}
+              className="px-3 py-4 bg-[#00684A] rounded-lg text-white  flex-shrink-0"
+            >
+              Add
+            </button> */}
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {teamNames.map((teamName, index) => (
-            <div
-              key={index}
-              className="flex flex-col bg-white ring-1 ring-gray-100 gap-2 items-center border p-3 rounded-lg"
-            >
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {teamNames.map((teamName, index) => (
+          <div
+            key={index}
+            className="flex flex-col bg-white ring-1 ring-gray-100 gap-2 items-center border p-3 rounded-lg"
+          >
+            <input
+              type="text"
+              placeholder={`Team ${index + 1} Name`}
+              className="border p-3 font-poppins ring-2 ring-[#00684A] font-light rounded-lg w-full"
+              value={teamName}
+              onChange={(e) => handleTeamNameChange(index, e)}
+              required
+            />
+            <div className="relative flex w-full max-w-sm flex-col gap-1">
               <input
-                type="text"
-                placeholder={`Team ${index + 1} Name`}
-                className="border p-3 font-poppins ring-2 ring-[#00684A] font-light rounded-lg w-full"
-                value={teamName}
-                onChange={(e) => handleTeamNameChange(index, e)}
-                required
+                id={`fileInput-${index}`}
+                type="file"
+                accept="image/*"
+                className="w-full max-w-md rounded-lg text-black font-poppins file:mr-4 file:cursor-pointer file:border-none file:bg-[#00684A] hover:file:bg-white file:px-4 file:py-2 file:font-semibold file:text-white hover:file:text-[#00684A]  file:font-poppins ring-2 ring-[#00684A] transition-all "
+                onChange={(e) => handleLogoChange(index, e)}
               />
-              <div className="relative flex w-full max-w-sm flex-col gap-1">
-                <input
-                  id={`fileInput-${index}`}
-                  type="file"
-                  accept="image/*"
-                  className="w-full max-w-md rounded-lg text-black font-poppins file:mr-4 file:cursor-pointer file:border-none file:bg-[#00684A] hover:file:bg-white file:px-4 file:py-2 file:font-semibold file:text-white hover:file:text-[#00684A]  file:font-poppins ring-2 ring-[#00684A] transition-all "
-                  onChange={(e) => handleLogoChange(index, e)}
-                />
-              </div>
             </div>
-          ))}
-        </div>
-        {error && <p className="text-red-700 text-sm">{error}</p>}
-        <button
-          type="submit"
-          className="self-end px-8 py-2 bg-[#00684A] ring-2 hover:ring-[#00684A] hover:bg-white font-semibold font-poppins text-white hover:text-[#00684A] rounded-lg uppercase hover:opacity-95 mt-4 transition-all"
-        >
-          Update Table
-        </button>
-      </form>
-    </main>
+          </div>
+        ))}
+      </div>
+      {error && <p className="text-red-700 text-sm">{error}</p>}
+      <button
+        type="submit"
+        className="self-end px-8 py-2 bg-[#00684A] ring-2 hover:ring-[#00684A] hover:bg-white font-semibold font-poppins text-white hover:text-[#00684A] rounded-lg uppercase hover:opacity-95 mt-4 transition-all"
+      >
+        Update Table
+      </button>
+    </form>
+  </main>
+  
+  
   );
 }
