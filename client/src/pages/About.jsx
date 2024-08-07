a0efdb24d1f29daf81c18cc86b62c87c2c80320d
Kidus Asebe <kidusasebe2019@gmail.com>
2024-08-07 12:42:09 +0300

    faq setup

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div className='px-4 py-12 max-w-2xl mx-auto'>
-      <h1 className='text-3xl font-bold  mb-4 text-slate-800'>About</h1>
-      <p className='mb-4 text-slate-700'>
-       Soccer table
-      </p>
-      
+    <div>
+      <nav className="bg-white px-4 lg:px-6 py-2.5">
+        <div className="flex flex-col lg:flex-row justify-between items-center mx-auto max-w-screen-xl">
+          <a href="/about" className="font-poppins hover:text-[#00684A] text-[80px] font-bold text-xl px-4 leading-[100px] lg:px-5 py-2 lg:py-2.5">
+            Frequently <br /> Asked <br />questions
+          </a>
+          <a href="/profile" className="mt-2 lg:mt-0">
+            <button className="bg-[#00684A] hover:ring-2 hover:ring-[#00684A] hover:bg-white hover:text-[#00684A] text-xl font-poppins rounded-lg text-white px-4 lg:px-16 py-2 lg:py-2.5 transition-all">
+              Sign in
+            </button>
+          </a>
+        </div>
+      </nav>
     </div>
   );
 }
