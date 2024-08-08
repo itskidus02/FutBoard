c71aa56fdbb4b6a05d7399e0834d9d6bf57bf94e
Kidus Asebe <kidusasebe2019@gmail.com>
2024-08-08 11:35:00 +0300

    manage matches slightly done

--- client/src/pages/ManageMatches.jsx
+++ client/src/pages/ManageMatches.jsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import DisplayTable from "./DisplayTable";
 
 export default function ManageMatches() {
   const { tableId } = useParams();
+  const [table, setTable] = useState(null);
   const [clubs, setClubs] = useState([]);
   const [selectedClubs, setSelectedClubs] = useState({
     homeClub: "",
@@ -19,21 +19,21 @@ export default function ManageMatches() {
   const [error, setError] = useState("");
 
   useEffect(() => {
-    // Fetch clubs from the table
-    const fetchClubs = async () => {
+    const fetchTableDetails = async () => {
       try {
         const response = await fetch(`/api/table/get/${tableId}`);
         if (!response.ok) {
-          throw new Error("Failed to fetch clubs");
+          throw new Error("Failed to fetch table details");
         }
-        const table = await response.json();
-        setClubs(table.clubs.map((c) => c.clubId)); // Extract club information
+        const tableData = await response.json();
+        setTable(tableData); // Set table details including name
+        setClubs(tableData.clubs.map((c) => c.clubId)); // Extract club information
       } catch (error) {
-        setError("Error fetching clubs: " + error.message);
+        setError("Error fetching table details: " + error.message);
       }
     };
 
-    fetchClubs();
+    fetchTableDetails();
   }, [tableId]);
 
   const handleClubChange = (type, clubId) => {
@@ -98,14 +98,16 @@ export default function ManageMatches() {
     (club) => club._id !== selectedClubs.homeClub
   );
 
-  return (
+  return  (
     <div className="w-full sm:w-3/4 mx-auto p-6 my-7">
-      <h1 className="text-2xl sm:text-3xl font-semibold text-center mb-6 sm:mb-8">
-        Manage Matches {}
-      </h1>
+      {table && (
+        <h1 className="text-2xl text-[#00684A] font-fraunces sm:text-3xl font-semibold text-center mb-6 sm:mb-8">
+          {table.name}
+        </h1>
+      )}
       <form
         onSubmit={handleMatchSubmit}
-        className="mt-8 w-full sm:w-3/4 lg:w-2/4 mx-auto ring-2 ring-blue-600 rounded px-4 sm:px-8 pt-6 pb-8 mb-4"
+        className="mt-8 w-full sm:w-3/4 lg:w-2/4 mx-auto ring-2 ring-[#00684A] rounded px-4 sm:px-8 pt-6 pb-8 mb-4"
       >
         <div className="mb-4 flex flex-col sm:flex-row justify-between">
           <div className="w-full sm:w-1/2 sm:pr-2 mb-4 sm:mb-0">
@@ -114,9 +116,9 @@ export default function ManageMatches() {
                 id="homeClub"
                 value={selectedClubs.homeClub}
                 onChange={(e) => handleClubChange("homeClub", e.target.value)}
-                className="block appearance-none w-full sm:w-3/4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-4 sm:mb-0"
+                className="block appearance-none w-full sm:w-3/4 ring-2 ring-[#00684A] font-poppins border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-4 sm:mb-0"
               >
-                <option value="j">Select Home Club</option>
+                <option value="">Select Home Club</option>
                 {filteredHomeClubs.map((club) => (
                   <option key={club._id} value={club._id}>
                     {club.name}
@@ -130,18 +132,17 @@ export default function ManageMatches() {
                 onChange={(e) =>
                   handleMatchResultChange("homeGoals", e.target.value)
                 }
-                className="appearance-none block w-full sm:w-1/4 ml-0 sm:ml-2 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="appearance-none block ring-2 ring-[#00684A] font-poppins w-full sm:w-1/4 ml-0 sm:ml-2 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               />
             </div>
           </div>
           <div className="w-full sm:w-1/2 sm:pl-2">
             <div className="flex flex-col gap-3 sm:flex-row items-center">
-              
               <select
                 id="awayClub"
                 value={selectedClubs.awayClub}
                 onChange={(e) => handleClubChange("awayClub", e.target.value)}
-                className="block appearance-none w-full sm:w-3/4 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="block appearance-none w-full sm:w-3/4 ring-2 ring-[#00684A] font-poppins   py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               >
                 <option value="">Select Away Club</option>
                 {filteredAwayClubs.map((club) => (
@@ -157,37 +158,36 @@ export default function ManageMatches() {
                 onChange={(e) =>
                   handleMatchResultChange("awayGoals", e.target.value)
                 }
-                className="appearance-none block w-full sm:w-1/4 ml-0 sm:ml-2 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-4 sm:mb-0"
+                className="appearance-none block w-full sm:w-1/4 ml-0 sm:ml-2 ring-2 ring-[#00684A] font-poppins rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-4 sm:mb-0"
               />
             </div>
           </div>
         </div>
-        <div className="mb-4">
+        <div className="mb-4 flex flex-col items-center">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-gray-700 font-poppins text-sm font-bold mb-2"
             htmlFor="matchDate"
           >
-            Match Date
+            Pick a Date
           </label>
           <input
             type="date"
             id="matchDate"
             value={matchDate}
             onChange={handleMatchDateChange}
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            className="appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           />
         </div>
         {error && <p className="text-red-500 text-xs italic">{error}</p>}
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-end">
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-[#00684A] hover:text-[#00684A] hover:bg-white hover:ring-2 hover:ring-[#00684A] transition-all text-white font-semibold py-2 px-4 rounded-lg"
           >
             Save Match Result
           </button>
         </div>
       </form>
-      <DisplayTable/>
       <ToastContainer
         position="bottom-right"
         autoClose={5000}
@@ -201,5 +201,4 @@ export default function ManageMatches() {
       />
     </div>
   );
-  
 }
