54b60d3df29b0a6806919214914db0184838ce60
Kidus Asebe <kidusasebe2019@gmail.com>
2024-08-09 00:05:23 +0300

    footer cant stick

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-black text-white py-8 flex flex-col items-center">
-      <div className="text-white text-[100px] lg:text-[200px] font-bold">
+    <footer className="w-full text-[#00684A] py-4 sm:py-8 flex flex-col items-center">
+      <div className="text-[#00684A] font-fraunces text-[50px] sm:text-[100px] md:text-[150px] lg:text-[200px] font-[700]">
         <div className="relative w-full max-w-screen-xl flex justify-center items-center">
-          YourBrand
-          <span className="absolute left-0 bottom-0 text-[10px] sm:text-[14px]">
+          FutBoard
+          <span className="absolute left-0 bottom-0 text-[8px] sm:text-[10px] md:text-[12px] lg:text-[14px]">
             © Your Company Pty. Ltd.
           </span>
-          
-          <div className="absolute right-0 bottom-0 flex space-x-4">
-            <button className="bg-white text-black px-4 py-2 text-[10px] sm:text-[14px]">
-              Button 1
+          <div className="absolute right-0 bottom-0 flex space-x-2 sm:space-x-4">
+            <button className="ring text-black px-2 py-1 text-[8px] sm:text-[10px] md:text-[12px] lg:text-[14px]">
+           UP
             </button>
-            <button className="bg-white text-black px-4 py-2 text-[10px] sm:text-[14px]">
-              Button 2
+            <button className="ring text-black px-2 py-1 text-[8px] sm:text-[10px] md:text-[12px] lg:text-[14px]">
+              GIT
             </button>
           </div>
         </div>
