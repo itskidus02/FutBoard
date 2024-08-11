d5dd53905b1035c6ed7a2809ccd62d0df0b00baf
Kidus Asebe <kidusasebe2019@gmail.com>
2024-08-11 23:55:55 +0300

    footer minor change

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
 
   return (
     <footer className="w-full text-[#00684A] py-4 sm:py-8 flex flex-col items-center">
-      <div className="text-[#00684A] font-fraunces text-[100px] sm:text-[100px] md:text-[150px] lg:text-[200px] font-[700]">
+      <div className="text-[#00684A] font-fraunces text-[90px] sm:text-[100px] md:text-[150px] lg:text-[200px] font-[700]">
         <div className="relative w-full max-w-screen-xl flex justify-center items-center">
           FutBoard
           <span className="absolute left-0 bottom-0 text-[8px] sm:text-[10px] md:text-[12px] lg:text-[14px]">
