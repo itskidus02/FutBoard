064e1454359b038df6afb2dbfdf3e49c9a72eb20
Kidus Asebe <kidusasebe2019@gmail.com>
2024-08-12 21:42:13 +0300

    added data on faq

--- client/src/components/Demo/AccordionDemo.jsx
+++ client/src/components/Demo/AccordionDemo.jsx
@@ -1,40 +1,74 @@
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion";
-  
-  export function AccordionDemo() {
-    return (
-      <div className="flex text-[20px] font-poppins flex-col justify-center items-center ">
-        <Accordion type="single" collapsible className="w-5/6  mx-auto">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>How long does the registration process take?</AccordionTrigger>
-            <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger>Can I add or remove employees at any time??</AccordionTrigger>
-            <AccordionContent>
-              Yes. It comes with default styles that match the other components' aesthetic.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger>Is there a limit to the number of bookings I can receive??</AccordionTrigger>
-            <AccordionContent>
-            No, there are no limits on the number of bookings you can receive through the platform. Our system is designed to scale to handle your business needs, whether you're a small operation or a large enterprise.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-4">
-            <AccordionTrigger>Can I integrate the booking system with my existing website or software??</AccordionTrigger>
-            <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </div>
-    );
-  }
-  
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+export function AccordionDemo() {
+  return (
+    <div className="flex  font-poppins flex-col items-center">
+      <Accordion type="single" collapsible className="w-5/6 mx-auto">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>How do I create a new table?</AccordionTrigger>
+          <AccordionContent>
+            To create a new table, navigate to the "create table" button from
+            home page or profile page. Fill in the required information such as
+            table name, number of teams, team names, club logos, and any other
+            relevant details, then save the changes.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>
+            How can I update or edit a team's information?
+          </AccordionTrigger>
+          <AccordionContent>
+            To update a table and team's information, go to the "profile"
+            section, find the table you wish to edit, and click on the "Update
+            table" button. Make the necessary changes and save the updated
+            information.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>
+            What should I do if I need to delete a table?
+          </AccordionTrigger>
+          <AccordionContent>
+            To delete a table, go to the "profile" section, locate the team you
+            want to remove, and click on the "Delete table" button. Confirm the
+            deletion when prompted. Please note that deleting a table will also
+            remove any associated data.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-4">
+          <AccordionTrigger>
+            How do I create a new match between clubs?
+          </AccordionTrigger>
+          <AccordionContent>
+            To create a new match, go to the "profile" section and choose the
+            desired table and click "manage Matches". Once there use the
+            dropdown menus to select the clubs participating in the match. Enter
+            the match details such as date and time, and goals scored in that
+            match. then save the match.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-5">
+          <AccordionTrigger>
+            Can I update or delete a match once it is created?
+          </AccordionTrigger>
+          <AccordionContent>No you cant</AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-6">
+          <AccordionTrigger>
+            How can I manage tables and standings for the teams?
+          </AccordionTrigger>
+          <AccordionContent>
+            All you have to do is create table and teams under that table. and
+            after that create matches between those teams and FutBoard will do
+            the rest for you
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+}
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,20 +1,18 @@
-// import { Accordion } from '@/components/ui/accordion';
 import { AccordionDemo } from "@/components/Demo/AccordionDemo";
 import React from "react";
 
 export default function About() {
   return (
-    <div>
-      <nav className="bg-white px-4 lg:px-6 py-2.5">
-        <div className="flex flex-col lg:flex-row justify-between items-center mx-auto max-w-screen-xl">
+    <div className="flex flex-col items-center justify-center bg-white">
+      <nav className="w-full px-4 lg:px-6 py-2.5">
+        <div className="flex flex-col lg:flex-row mx-auto max-w-screen-xl w-full">
           <a
-            className="font-fraunces lg:text-[90px] md:text-[60px] font-bold text-4xl px-4 lg:leading-[100px] md:leading-[60px]  lg:px-5 py-2 lg:py-2.5"
+            className="font-fraunces text-center lg:text-[90px] md:text-[60px] font-bold text-4xl px-4 lg:leading-[100px] md:leading-[60px] lg:px-5 py-2 lg:py-2.5"
           >
             Frequently <br /> Asked <br /> <span className="text-[#00684A]">Questions</span>
           </a>
-          <div className="mt-2 lg:mt-0 lg:self-start">
+          <div className="mt-4 w-full">
             <AccordionDemo />
-
           </div>
         </div>
       </nav>
